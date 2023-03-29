b8463e07ff1a41e1f6631840e3047a44a9fb8057
Javier Murua <javierantoniomurua@gmail.com>
2023-03-29 18:39:24 -0300

    Agregados los cambios para el despliegue

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://JavierMurua.github.io',
+  base: '/fazt-landingpage2',
   integrations: [tailwind()]
 });
